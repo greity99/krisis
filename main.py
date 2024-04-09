fbad9cf6cbaa950543e728a3f5ec4aab41f328ce
Geemmeet <ellen.gemback@gmail.com>
2024-04-09 15:07:33 +0200

    Upd main

--- main.py
+++ main.py
@@ -2,33 +2,34 @@
 from bottle import run, template, route
 import route, run, template, static_file, request, redirect
 import psycopg2
-
+ 
+'''
 #General variables
 host = "pgserver.mau.se"
 database = "XXX"
 user = "XXX"
 password = "XXX"
 port = "5432"  # Default PostgreSQL port
-
+'''
 
 
 @route("/")
 def index():
-    return template("index")
+    return template("index.html")
 
-@route("/views/krisklar_tips.html")
+@route("/Kristips")
 def chrisis_tips():
-    return template("krisklar_tips")
+    return template("chrisis_tips.html")
 
-#Denna är lite oklar, vet inte varför det finns två publish post/publicera inlägg, återkommer
-@route("/view/publicera_inlägg.html")
+@route("/Skapa inlägg")
 def publish_post():
-    return template("publicera_inlägg")
+    return template("publish_post.html")
 
-@route("/views/login.html")
+@route("/logga in")
 def login():
-    return template("login")
+    return template("login.html")
 
+'''
 #Connect to PostgreSQL
 try: 
     conn = psycopg2.connect(
@@ -45,5 +46,6 @@ try:
     
 except psycopg2.Error as error: 
     print(f"Error: unable to connect to the database\n {error}")
-    
+'''
+
 run(host="127.0.0.1", port=8080)
