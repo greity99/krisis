19ccac1b3c24bff0f09b2a22cbe28bb8e4a2cb8b
MalikTallouzi <maliktallouzi@hotmail.com>
2024-05-28 10:27:29 +0200

    Bekräfta ändring av mejl popup

--- static/script.js
+++ static/script.js
@@ -6,4 +6,60 @@ function togglePassword() {
   } else {
     x.type = "password";
   }
+<<<<<<< Updated upstream
 }
+=======
+}
+
+
+//Function which makes the input field for updating the email-address on the profile page available and changes the name and type of the button when clicked.
+//Keeps track of the buttons state
+var isEmailFieldEnabled = false;
+function undisable_emailfield() {
+  var emailField = document.getElementById("email_field");
+  var btnToggle = document.getElementById("btn_toggle");
+
+  if (!isEmailFieldEnabled) {
+    emailField.removeAttribute("disabled");
+    btnToggle.value = "Spara";
+    isEmailFieldEnabled = true;
+  } else {
+    openSettingsModal()
+  }
+}
+
+//Function which is used on the profile page to open a modal
+function openDeleteModal() {
+  var modal = document.getElementById("deleteModal");
+  modal.style.display = "block";
+}
+
+function closeDeleteModal() {
+  var modal = document.getElementById("deleteModal");
+  modal.style.display = "none";
+}
+
+
+//Function which is used on the logout-button to open a modal
+function openLogoutModal() {
+  var modal = document.getElementById("logoutModal");
+  modal.style.display = "block";
+}
+
+function closeLogoutModal() {
+  var modal = document.getElementById("logoutModal");
+  modal.style.display = "none";
+}
+
+//Function which is used on the "save email button" to open a modal
+function openSettingsModal() {
+  var modal = document.getElementById("saveSettingsModal");
+  modal.style.display = "block";
+}
+
+function closeSettingsModal() {
+  var modal = document.getElementById("saveSettingsModal");
+  modal.style.display = "none";
+}
+
+>>>>>>> Stashed changes
